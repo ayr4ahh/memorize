e70826a618871cf416a9381f4123fb0b117b8241
Ayr4ahh <213103689+ayr4ahh@users.noreply.github.com>
2025-05-24 13:27:35 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Ayaan Ackerman on 5/24/25.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
